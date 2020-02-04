c8df9150fc311fb4fb3a0d1060398e43659aa00e
Tyler OLeary <tylero@pop-os.localdomain>
2020-02-04 15:55:31 -0800

    message words

---
+++ Proj1/Proj1.java
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+// Create Scanner for input
+// Create Array list to store problem
+// Create Program loop with exit code
+// Print out instructions
+
+    //Loop for input, While loop with exit code?
+        // - Add opperator/opperand to list after every enter
+
+// Error Checking Sequence
+    // Run throught and check for proper parenthisis - loop
+        // - They have pairs, have stuff between them
+        // - If error, throw perenthisis error
+    // Run through and check for propper opperators - loop
+        // - have numbers between them
+        // - Throw error if wrong
+    // Run through and check for proper opperands - loop
+        // - have oppereators between, no double decimals, ect...
+        // - Throw error if wrong
+    // Run through and check for complex opperands (sqrt, pow, ect) - loop
+        // - Correct spelling, have stuff in them
+        // - throw error if wrong.
+
+// Do Problem - follow PMDAS
+    // Parenthisis first - loop
+        // find parenthisis pair, then do the opperation in the middle
+        // Delete the opperands/opperators, replace with answer
+    // Do mult/div - loop
+        // including sqrt, pow
+    // Do addition and subtraction - loop
+
+// Output the answer! -Array list will be size 1 by the end
+    // prompt for another problem, or go straight into it
