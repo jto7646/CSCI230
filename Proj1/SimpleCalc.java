3c2732db59393789e3b6ccf7ef7d903f43ffc3cc
Tyler OLeary <tylero@pop-os.localdomain>
2020-02-06 17:26:38 -0800

    words!!

--- Proj1/SimpleCalc.java
+++ Proj1/SimpleCalc.java
@@ -11,14 +11,34 @@ public class SimpleCalc {
         ArrayList list = new ArrayList<>();     // Used to store user input
         Boolean endProg = false;                // Used as exit perameter
         Boolean struct = true;                  // Determines if first time instructions are displayed
+        Boolean compute = false;                // Used to exit the user input loop
+        Boolean testing = true;
         int test = 0;//TESTING
 
-        while(!endProg){
+        while(!endProg){ // Loop holding the program
             if(struct){ // First time instructions
                 System.out.println("Simple clculator program:\nPress enter after each opperator or opperand\nCan use parenthesis\n");
                 System.out.println("Special opperators:\nSquare root: sqrt Exponent: pow");
+                System.out.println("*****************************************************");
                 struct = false;
             }
+
+            while(!compute){    // Loop for user input
+                list.add(input.next()); // adds an element to the arrayList
+                if( (list.get(list.size()-1)) == "e "){compute = true;}; // Checks if last entered is "e", if so breaks loop
+            }
+            compute = false;
+            list.remove(list.size()-1); // removes the "e" from the end of the arraylist
+
+            // Make loop for inner test loops!!
+            while(testing){     // Loop used for error checking
+
+                for(int i = 0; i <list.size(); i++){ // Loop used for checking for parenthisis errors
+                    
+                }
+
+            }
+            testing = true;
             //if(test == 3){endProg = true;}//TESTING
            // test++; //TESTING
         }
@@ -37,7 +57,7 @@ public class SimpleCalc {
 // Create Program loop with exit code   !DONE!
 // Print out instructions
 
-    //Loop for input, While loop with exit code?
+    //Loop for input, While loop with exit code?   !DONE! 
         // - Add opperator/opperand to list after every enter
 
 // Error Checking Sequence
