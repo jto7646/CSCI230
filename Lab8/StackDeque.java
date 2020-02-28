24099b72ce8d228043969a7971109ffe2f90fddf
Tyler OLeary <tylero@pop-os.localdomain>
2020-02-27 17:32:02 -0800

    Words

---
+++ Lab8/StackDeque.java
@@ -0,0 +1,12 @@
+import net.datastructures.ArrayStack;
+import net.datastructures.Deque;
+
+
+public class StackDeque<E> implements Deque<E> {
+
+    private ArrayStack<E> frontStack, backStack;
+    // reverse the front stack into the back stack, use reverse to access the back, normal to access the front
+    public int size(){
+        return frontStack.size();
+    }
+}
