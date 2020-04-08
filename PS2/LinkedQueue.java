c8ea747fb3dc3266c003f20639e9ee181236601c
jto7646 <j.oleary9906@student.sbccd.edu>
2020-04-08 15:48:32 -0700

    Project two is complete!!!

---
+++ PS2/LinkedQueue.java
@@ -0,0 +1,131 @@
+//import jdk.internal.jimage.ImageReader.Node;
+
+/*
+ * Copyright 2014, Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser
+ *
+ * Developed for use with the book:
+ *
+ *    Data Structures and Algorithms in Java, Sixth Edition
+ *    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+ *    John Wiley & Sons, 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+//package net.datastructures;
+
+/**
+ * Realization of a FIFO queue as an adaptation of a SinglyLinkedList.
+ * All operations are performed in constant time.
+ *
+ * @author Michael T. Goodrich
+ * @author Roberto Tamassia
+ * @author Michael H. Goldwasser
+ * @see SinglyLinkedList
+ */
+public class LinkedQueue<E> implements Queue<E> {
+
+  /** The primary storage for elements of the queue */
+  private SinglyLinkedList<E> list = new SinglyLinkedList<>();   // an empty  list
+
+  /** Constructs an initially empty queue. */
+  public LinkedQueue() { }                  // new queue relies on the initially empty list
+
+  /**
+   * Returns the number of elements in the queue.
+   * @return number of elements in the queue
+   */
+  @Override
+  public int size() { return list.size(); }
+
+  /**
+   * Tests whether the queue is empty.
+   * @return true if the queue is empty, false otherwise
+   */
+  @Override
+  public boolean isEmpty() { return list.isEmpty(); }
+
+  /**
+   * Inserts an element at the rear of the queue.
+   * @param element  the element to be inserted
+   */
+  @Override
+  public void enqueue(E element) { list.addLast(element); }
+
+  /**
+   * Returns, but does not remove, the first element of the queue.
+   * @return the first element of the queue (or null if empty)
+   */
+  @Override
+  public E first() { return list.first(); }
+
+
+  /**
+   * Removes and returns the first element of the queue.
+   * @return element removed (or null if empty)
+   */
+  @Override
+  public E dequeue() { return list.removeFirst(); }
+
+  /** Produces a string representation of the contents of the queue.
+   *  (from front to back). This exists for debugging purposes only.
+   */
+  public String toString() {
+    return list.toString();
+  }
+
+  /**
+   * Appends Q2 to Q1 in constant time
+   * @param Q1 to append to 
+   * @param Q2 being appended
+   */
+  public void concatenate(LinkedQueue<E> Q1, LinkedQueue<E> Q2) {
+    // Appends Q2 to Q1 in constant-time
+    list.concantinate(Q1.list, Q2.list);
+
+    assert Q2.isEmpty() : "Error: Q2 should be empty!";
+    }
+
+    // Bellow main function used for testing
+    
+    public static void main(String[] args) {
+      LinkedQueue test = new LinkedQueue<Integer>();
+      LinkedQueue test2 = new LinkedQueue<Integer>();
+
+      test.enqueue(13);
+      test.enqueue(15);
+      test.enqueue(24);
+
+      test2.enqueue(1);
+      test2.enqueue(2);
+      test2.enqueue(3);
+      test2.enqueue(4);
+      test2.enqueue(5);
+
+
+      test.concatenate(test, test2);
+
+      System.out.println(test.toString());
+
+      System.out.println(test2.toString());
+
+      for(int i = 0; i < 7; i++){ test.dequeue();}
+
+      System.out.println(test.toString());
+
+
+    }
+    
+
+
+}// ************ END OF CLASS *************
--- Proj2/GuitarHero.java
+++ Proj2/GuitarHero.java
@@ -1,47 +1,62 @@
 import java.lang.Math;
-import java.lang.reflect.Array;
-import java.lang.Thread;
+
 //import java.util.Arrays;
 
 /**
- * GuitarHero
+ * GuitarHero: Hit the keys to make beautiful rock music!
  * @author John OLeary
- * @version 1.0
- * @since March/25/2020
+ * @version 2.0
+ * @since April/8th/2020
  */
 public class GuitarHero {
 
     public static void main(String[] args) {
-        int index = 0;
-        int indCheck = -1;
-        Double frequencyFiller = 0.00;
-        Double sample = 0.00;
+        int index = 0; // Stores the index of key in keyboard
+        int indCheck = -1; // Used to check if valid key index found
+        Double frequencyFiller = 0.00; // Used for filling the array 
+        Double sample = 0.00;   // Stores the current audio sample
         GuitarString[] keyValues = new GuitarString[37];  // Array holding 37 GuitarStrings for the keyboard
 
         
-       // Bellow for graph drawing
+       // ***** Bellow for graph drawing ****
         StdDraw.setPenRadius(.0035);
-        StdDraw.setScale(0.0, 100.0);
+        // VV Makes draw area 100X100 pixels
+        StdDraw.setScale(0.0, 100.0);   
         StdDraw.setPenColor(StdDraw.RED);
-        double drawIndex = 0.0;
-        double temp = 0.00;
+        // VV Used as a timer to hide the graph as it draws
+        double drawIndex = 0.0; 
+        // VV Temp storage for the sample used as starting point for next line draw       
+        double temp = 0.00;             
         StdDraw.enableDoubleBuffering();
+        // VV Used as a delay time for the image to start drawing,
+        // VV needed for the program to run smoothly
+        double drawDelay = 0.00;
+        // ***^^^**********************^^^***
 
-       
+        // VV This string holds the valid key presses. The users input is checked 
+        // VV against this string to validate the key press
         String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
-        char key = 'f';
+        char key = 'f'; // Stores user input
 
+        // VV The GuitarString array is filled with GuitarStrings of different frequencies
         for(int i = 0; i < 37; i++){
             frequencyFiller = 440.0 * Math.pow(1.05956, (i - 24.0) ); 
             keyValues[i] = new GuitarString(frequencyFiller);
         }
         
 
-        while (true) {
+    
+        while (true) { // Program body loop 
            
+            // VV Checks for key presses, initiates if a key is pressed
             if(StdDraw.hasNextKeyTyped()){
+                // VV stores the pressed key as character
                 key = StdDraw.nextKeyTyped();
-                indCheck = indexOf(keyboard, key);
+                // VV Tries to find the key in the keyboard string, if found will return
+                // VV the index, if not found will return -1 
+                indCheck = indexOf(keyboard, key); 
+                // VV Plucks the tring at the location within the array using the index from the 
+                // VV keyboard string as the array index, if key was found in the string 
                 if(indCheck != -1){
                     index = indCheck;
                     keyValues[index].pluck();
@@ -49,32 +64,52 @@ public class GuitarHero {
             }
             sample = keyValues[index].sample();
             
-            // SuperPosition
+            // VV Calculates the superposition of the plucked strings sample
             for(int i = 0; i < 37; i++){
                 sample +=keyValues[i].sample();
             }
 
-
+            // VV Plays the audio sample of the plucked string
             StdAudio.play(sample);
 
-             // New Stuff
-             StdDraw.line((drawIndex), (50.0 + (temp * 10)), (drawIndex + 1), (50.0 + (sample * 10)));
-             if(drawIndex == 99){
-                StdDraw.show();
-                StdDraw.clear();}
-             drawIndex = (drawIndex + 1) % 100;
-             temp = sample;
-             // ^^^^^^^^
+            
 
+            // ***** Bellow for graph drawing **** 
+            // VV This bit of code draws 100 little line segments using coordinates derived from the currently
+            // VV playing audio sample. Each little line is drawn from the end of the first one to the value stored 
+            // VV in the sample (times ten to make it a little bigger). The Y coordinates have 50 added to them to 
+            // VV make the lines draw close to the center of the 100 X 100 pixel box.
+            if(drawDelay % 5 == 0){ //Delay for smooth running
+                // VV The little line being drawn
+                StdDraw.line((drawIndex), (50.0 + (temp * 10)), (drawIndex + 1), (50.0 + (sample * 10)));
+                // VV Only displays the graph after all of the lines are drawn
+                if(drawIndex == 99){
+                    StdDraw.show();// Shows hidden display buffer
+                    StdDraw.clear();} // Hides display buffer again
+                drawIndex = (drawIndex + 1) % 100;
+                temp = sample; // Stores end of previos line
+            }
+            // ***^^^**********************^^^***
 
+            // VV Advances the guitar string simulation by one step for all strings
            for(int i = 0; i < 37; i++){
                 keyValues[i].tic();
              }
-        }//END WHILE  
-    } // END MAIN
 
+             drawDelay++; // Draw delay incrementation
+
+        }//END OF BODY LOOP 
+    } // **** END OF MAIN ****
+
+    /**
+     * Returns the index of a given character as an int value within a given String. Returns -1 if the character is not found
+     * @param string
+     * @param find
+     * @return index if found, -1 if not 
+     */
     public static int indexOf(String string, char find){
 
+        // VV Iterates through the string, returning the index where the character is found
         for(int i = 0; i < string.length(); i++){
             if(string.charAt(i) == find){return i;}
         }
