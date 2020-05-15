a272a921addd99a6b9df5d500203d11e8c327859
Tyler OLeary <tylero@pop-os.localdomain>
2020-05-14 19:29:23 -0700

    Keepen on truckin

--- Proj4/TerminalDriver.java
+++ Proj4/TerminalDriver.java
@@ -3,7 +3,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
 import java.util.TreeMap;
-import java.util.Map.Entry;
 import java.util.ArrayList;
 
 public class TerminalDriver {
@@ -12,18 +11,20 @@ public class TerminalDriver {
 
 
     public static void main(String[] args) {
-        boolean running = true;
-        boolean dirGood = false;
-        
-        TreeMap<Integer, Integer> hashTree = new TreeMap<>();
-        ArrayList<File> files = new ArrayList<>();
-        Scanner userInput = new Scanner(System.in);
-        String userString = new String();
-        InputStream comparing, compareTo;
-        int userInt = 0, fileSelect = 0;
-        File folder;
-        TreeBuilder hashBuilder = new TreeBuilder();
-        WordSearch compareFiles = new WordSearch();
+        boolean running = true; // Main Loop bool
+        boolean dirGood = false; // Directory select bool
+        ArrayList<TreeMap> treeArray = new ArrayList<>(); // holds hash trees for all files 
+        TreeMap<Integer, Integer> hashTree = new TreeMap<>(); // creates the hash trees
+        ArrayList<File> files = new ArrayList<>();  // Holds the files for comparison
+        ArrayList<MatchSet> matchSets = new ArrayList<>(); // Holds the file matches 
+        Scanner userInput = new Scanner(System.in); // Used for user input
+        String userString = new String();   // Used for user input
+        int userInt = 0;    // Used for user input 
+        InputStream comparing; // Reads the files 
+        int sequenceSize = 0; // Dictates the size of the word sequences being compared 
+        File folder; // Directory path 
+        TreeBuilder hashBuilder = new TreeBuilder(); // creates the hash trees
+        WordSearch compareFiles = new WordSearch(); // compares the two files, needed?
         
         // Program main loop, Needed?
         while(running){
@@ -79,23 +80,37 @@ public class TerminalDriver {
 
                 // Make an array of hashMaps, one from each file, then itterate through them for comparison
 
-                try {
-                    // Path to the first file
-                    comparing = new FileInputStream(files.get(fileSelect).getPath());
-                    // Use bellow
-                    for (int key : hashTree.keySet()) {
+                // Make Array of hash trees
+                for (int i = 0; i < files.size(); i++) {
+                    try {
+                        comparing = new FileInputStream(files.get(i).getPath());
+                        // Build a hash tree for the file
+                        hashBuilder = new TreeBuilder(comparing, sequenceSize);
+                        hashTree = hashBuilder.build();
+                        // Add the hashTree to the array
+                        treeArray.add(hashTree);
+
+                    } catch (Exception e) {
+                        System.out.println("File read error - Tree Building: " + e);
+                    }
+
+                }
+
+                // Compare the hash trees
+                for (int i = 0; i < files.size(); i++) {
+                    // Current file being compared 
+                    hashTree = treeArray.get(i);
+
+                    // Compare current file to all of the others
+                    for (int j = i+1; j < files.size(); j++) {  
                         
+                        for (Object key : treeArray.get(j).keySet()) {
+                            // TODO: if current key is found in hashTree, iterate 
+                        }
+                        // TODO: add count from above to arrayList with MatchSets
                     }
-                    // Compare first file with the rest of the files
-                    while(true){
-                    
-                    }
 
 
-
-
-                } catch (Exception e) {
-                    System.out.println("File read error: " + e);
                 }
 
             }
@@ -108,6 +123,7 @@ public class TerminalDriver {
         System.out.println("End Program");
     }
 
+
     public class MatchSet {
         private int match;
         private String fileNames;
@@ -116,6 +132,11 @@ public class TerminalDriver {
             this.match = match;
             this.fileNames = fileNames; 
         }
+
+        public String getFileNames(){return fileNames;}
+
+        public int getMatchCount(){return match;}
+
     }
 
 
